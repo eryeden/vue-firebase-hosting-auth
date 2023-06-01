0c377988178db8c9484d017bca42a9f38982eaaa
eryeden <ery.eden@gmail.com>
2023-06-02 00:17:51 +0900

    display user name and id

--- vue-fastapi/src/components/Api.vue
+++ vue-fastapi/src/components/Api.vue
@@ -1,8 +1,6 @@
 <script>
 
-//import axios from "axios"
-import {axios} from "@/plugins/axios"
-import {getAuth} from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default {
     name: "Api",
@@ -12,72 +10,53 @@ export default {
     data() {
         return {
             text: null,
-            api_response: "",
-            token: ""
+            user_name: "",
+            user_id: ""
         }
     },
     mounted() {
-        axios.get("hello").then(
-            (response) => (this.text = response.data)
-        ).catch(
-            (e) => console.log(e)
-        );
+        const auth = getAuth();
+/*         const user = auth.currentUser;
+        if (user !== null) {
+            // The user object has basic properties such as display name, email, etc.
+            const displayName = user.displayName;
+            const email = user.email;
+            const photoURL = user.photoURL;
+            const emailVerified = user.emailVerified;
+            // The user's ID, unique to the Firebase project. Do NOT use
+            // this value to authenticate with your backend server, if
+            // you have one. Use User.getToken() instead.
+            const uid = user.uid;
+            this.user_id = uid;
+            this.user_name = displayName;
+            console.log("User data:" + this.user_id);
+        } else {
+            console.log("Failed to fetch the user data.");
+        } */
+
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // The user object has basic properties such as display name, email, etc.
+                const displayName = user.displayName;
+                const email = user.email;
+                const photoURL = user.photoURL;
+                const emailVerified = user.emailVerified;
+                // The user's ID, unique to the Firebase project. Do NOT use
+                // this value to authenticate with your backend server, if
+                // you have one. Use User.getToken() instead.
+                const uid = user.uid;
+                this.user_id = uid;
+                this.user_name = displayName;
+                console.log("User data:" + this.user_id);
+            } else {
+                // User is signed out
+                // ...
+                console.log("Failed to fetch the user data.");
+            }
+        });
+
     },
     methods: {
-        sendUser() {
-            // task:
-            // 1. getIdToken内で更新した内容を表示できればOK: done
-            // 2. tokenとforce refreshせず、キャッシュする
-
-            const auth = getAuth();
-            // どうやらfirebase toolsで自動的にtoken cachingしており、期限ぎれなら更新するようだ。
-            // 自分でやる必要なし！
-            // refer to https://firebase.google.com/docs/reference/js/v8/firebase.User#getidtoken
-            this.token = auth.currentUser?.getIdToken(false).then(
-                // ここでアロー関数を使うのがポイント。普通の関数Objectだとスコープ外のthisをキャプチャーできなくなる。
-                // アロー関数だと、thisを明示的に持たないと一つ外のスコープのthisが採用される。
-                // Reference: https://open8tech.hatenablog.com/entry/2021/01/05/193818
-                (idToken) => {
-
-                    this.token = idToken
-                    console.log("ID token: " + this.token)
-
-                    axios.get("user", {
-                        headers: {
-                            "Authorization": "Bearer " + idToken
-                        }
-                    }).then(
-                        (response) => {
-                            console.log("API response:[/user]" + response)
-                            this.api_response = response
-                        }
-                    ).catch(
-                        (error) => {
-                            console.log("Error API response:[/user]" + error)
-                        }
-                    )
-                })
-        },
-        revokeToken() {
-            const auth = getAuth();
-            this.token = auth.currentUser?.getIdToken(false).then(
-                (idToken) => {
-                    this.token = idToken
-                    axios.get("revoke_token", {
-                        headers: {
-                            "Authorization": "Bearer " + idToken
-                        }
-                    }).then(
-                        (response) => {
-                            console.log("API response:[/revoke_token]" + response)
-                        }
-                    ).catch(
-                        (error) => {
-                            console.log("Error API response:[/revoke_token]" + error)
-                        }
-                    )
-                })
-        }
     },
 }
 
@@ -85,15 +64,9 @@ export default {
 
 
 <template>
-    <h3>API result</h3>
-    <br/>
-    <h4>{{ text }}</h4>
-    <br/>
-    <button @click="revokeToken">RevokeFirebaseToken</button>
-    <button @click="sendUser">USER</button>
-    <br/>
-    <h4>{{ token }}</h4>
-    <br/>
-    <h4>{{ api_response }}</h4>
-
+    <h3>User specific data</h3>
+    <br />
+    <h4>User name: {{ user_name }}</h4>
+    <br />
+    <h4>User id: {{ user_id }}</h4>
 </template>
